0f1bb130d3ba80b8c355abf47a4107377fecc197
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-05-07 22:03:01 +0530

    added dto

---
+++ src/main/java/com/app/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,9 @@
+package com.app.msscbeerservice.web.model;
+
+/**
+ * @author t0k02w6 on 07/05/21
+ * @project mssc-beer-service
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON;
+}
