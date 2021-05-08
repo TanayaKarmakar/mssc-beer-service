a9f10aca0fcf41ecc7c60c72427f98f77d9924dd
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-05-08 11:10:56 +0530

    added beer entity

--- src/test/java/com/app/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/com/app/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.app.msscbeerservice;
 
-import org.junit.jupiter.api.Test;
+import com.app.msscbeerservice.web.controller.BeerController;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@WebMvcTest(BeerController.class)
 class MsscBeerServiceApplicationTests {
+	MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void getBeerById() {
+		//mockMvc.perform(get())
+	}
+
+	@Test
+	void saveNewBeer() {
+
+	}
+
+	@Test
+	void updateBeerById() {
+
 	}
 
 }
