00f3a3fc683a63fcc47557778a0d964d5fac6bd4
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-05-19 07:45:00 +0530

    updated beer service to use the bom

--- src/test/java/com/app/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/com/app/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,28 +1,29 @@
-package com.app.msscbeerservice;
-
-import com.app.msscbeerservice.web.controller.BeerController;
-import org.junit.Test;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-@WebMvcTest(BeerController.class)
-class MsscBeerServiceApplicationTests {
-	MockMvc mockMvc;
-
-	@Test
-	void getBeerById() {
-		//mockMvc.perform(get())
-	}
-
-	@Test
-	void saveNewBeer() {
-
-	}
-
-	@Test
-	void updateBeerById() {
-
-	}
-
-}
+//package com.app.msscbeerservice;
+//
+//import com.app.msscbeerservice.web.controller.BeerController;
+////import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//@WebMvcTest(BeerController.class)
+//class MsscBeerServiceApplicationTests {
+//	MockMvc mockMvc;
+//
+//	@Test
+//	void getBeerById() {
+//		//mockMvc.perform(get())
+//	}
+//
+//	@Test
+//	void saveNewBeer() {
+//
+//	}
+//
+//	@Test
+//	void updateBeerById() {
+//
+//	}
+//
+//}
