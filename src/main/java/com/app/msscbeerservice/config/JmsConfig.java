57b6cfe79e69828047054461c40c99124887aefa
t0k02w6 <tanaya.karmakar@walmartlabs.com>
2021-05-24 08:58:05 +0530

    added jms properties

---
+++ src/main/java/com/app/msscbeerservice/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.app.msscbeerservice.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * @author t0k02w6 on 24/05/21
+ * @project mssc-beer-service
+ */
+@Configuration
+public class JmsConfig {
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
